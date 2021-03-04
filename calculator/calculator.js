67838ac2538b1439bff4efcbc4ab9edbca4a5c7f
shashidharnaik55 <shashidharnaik55@gmail.com>
2021-03-04 13:26:14 +0530

    calculator

---
+++ calculator/calculator.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+
+ $('.add').click(function (e){
+    e.preventDefault();
+    var sum1 = parseInt($("#num1").val()) + parseInt($("#num2").val());
+ $("#output").val(sum1);
+
+});
+$('.sub').click(function (e){
+    e.preventDefault();
+    var sum2 = parseInt($("#num1").val()) - parseInt($("#num2").val());
+$("#output").val(sum2);
+
+
+});
+$('.mul').click(function (e){
+    e.preventDefault();
+    var sum3 = parseInt($("#num1").val()) * parseInt($("#num2").val());
+$("#output").val(sum3);
+
+});
+$('.div').click(function (e){
+    e.preventDefault();
+    var sum4 = parseInt($("#num1").val()) / parseInt($("#num2").val());
+$("#output").val(sum4);
+
+});
+});
