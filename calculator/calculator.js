7be8c2e3176b8b7a010f05143d5847a6a436fd8e
shashidharnaik55 <shashidharnaik55@gmail.com>
2021-04-09 13:12:06 +0530

    style sheet updated

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,28 +1,22 @@
-$(document).ready(function(){
-
- $('.add').click(function (e){
-    e.preventDefault();
-    var sum1 = parseInt($("#num1").val()) + parseInt($("#num2").val());
- $("#output").val(sum1);
-
-});
-$('.sub').click(function (e){
-    e.preventDefault();
-    var sum2 = parseInt($("#num1").val()) - parseInt($("#num2").val());
-$("#output").val(sum2);
-
-
-});
-$('.mul').click(function (e){
-    e.preventDefault();
-    var sum3 = parseInt($("#num1").val()) * parseInt($("#num2").val());
-$("#output").val(sum3);
-
-});
-$('.div').click(function (e){
-    e.preventDefault();
-    var sum4 = parseInt($("#num1").val()) / parseInt($("#num2").val());
-$("#output").val(sum4);
-
-});
-});
+$(document).ready(function () {
+  $('#add').click(function (e) {
+    e.preventDefault()
+    var sum1 = parseInt($('#num1').val()) + parseInt($('#num2').val())
+    $('#output').val(sum1)
+  })
+  $('#sub').click(function (e) {
+    e.preventDefault()
+    var sum2 = parseInt($('#num1').val()) - parseInt($('#num2').val())
+    $('#output').val(sum2)
+  })
+  $('#mul').click(function (e) {
+    e.preventDefault()
+    var sum3 = parseInt($('#num1').val()) * parseInt($('#num2').val())
+    $('#output').val(sum3)
+  })
+  $('#divi').click(function (e) {
+    e.preventDefault()
+    var sum4 = parseInt($('#num1').val()) / parseInt($('#num2').val())
+    $('#output').val(sum4)
+  })
+})
